6095ea0f47077143c6581b4c324dca6ca84c0ae7
Lauris Kwan <kwanlauris@gmail.com>
2022-12-19 19:05:25 +0800

    basic layout

--- app.js
+++ app.js
@@ -13,11 +13,24 @@ function vh(percent) {
   );
   return (percent * h) / 100;
 }
+const nav = document.querySelector("nav");
+const canvasContainer = document.querySelector(".canvas-container")
+const canvasReal = document.querySelector("#canvas-real");
+const canvasDraft = document.querySelector("#canvas-draft");
+const ctxReal = canvasReal.getContext("2d");
+const ctxDraft = canvasDraft.getContext("2d");
 
-window.onload = () => {
-  const canvas = document.querySelector("canvas");
-  const ctx = canvas.getContext("2d");
-  // --- Resizing ---
-  canvas.width = vw(95);
-  canvas.height = window.innerHeight;
-};
+
+// --- Resize ---
+
+canvasReal.width = vw(95);
+canvasReal.height = window.innerHeight;
+canvasDraft.width = vw(95);
+canvasDraft.height = window.innerHeight;
+canvasContainer.style.width = `${canvasReal.width}px`;
+canvasContainer.style.height = `${canvasReal.height}px`;
+nav.style.width = `${window.innerWidth - canvasDraft.width}px`;
+nav.style.height = `${window.innerHeight}px`;
+
+
+
